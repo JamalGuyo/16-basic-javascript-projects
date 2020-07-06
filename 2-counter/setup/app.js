fce4706101aa13f4422a6e4a4bc2816016707020
JamalGuyo <jamalguyo08@gmail.com>
2020-07-06 21:14:25 +0300

    change color of value text based on value of count

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -6,7 +6,9 @@ let count = 0;
 // loop through buttons
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
+    //   check the class attribute values of the btn
     const styles = e.currentTarget.classList;
+    // change the count value based on btn selected
     if (styles.contains("decrease")) {
       count--;
     } else if (styles.contains("increase")) {
@@ -14,6 +16,15 @@ btns.forEach((btn) => {
     } else {
       count = 0;
     }
+    // change color based on count value
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "#102a41";
+    }
+    // change the value of the element to the new count value
     value.textContent = count;
   });
 });
