a72c80713ec71e13dd2efa3abc96010d7c1e4387
JamalGuyo <jamalguyo08@gmail.com>
2020-07-06 20:31:53 +0300

    add js logic

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// select the element to change its bg
+const body = document.querySelector("body");
+// select the span
+const span = document.querySelector(".color");
+// select button element
+const btn = document.querySelector("#btn");
+// generate random index of the array
+const randIdx = () => Math.floor(Math.random() * colors.length);
+// check button click event
+btn.addEventListener("click", () => {
+  // change the background color
+  const selectedColor = colors[randIdx()];
+  body.style.background = selectedColor;
+  span.textContent = selectedColor;
+});
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,24 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// element to change bg
+const body = document.querySelector("body");
+// element to change text
+const span = document.querySelector(".color");
+// btn element to click
+const btn = document.querySelector("#btn");
+// randomly select the 6 hex values
+const randHex = () => {
+  const newHexArr = ["#"];
+  for (let i = 0; i < 6; i++) {
+    newHexArr.push(hex[Math.floor(Math.random() * hex.length)]);
+  }
+  return newHexArr.join("");
+};
+// execute fn on clicking the btn
+btn.addEventListener("click", () => {
+  // random hex value generated
+  const newHex = randHex();
+  // change body bg
+  body.style.background = newHex;
+  // change span text
+  span.textContent = newHex;
+});
